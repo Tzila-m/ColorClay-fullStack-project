0e9e39f8b9493b6320da249097296a631e7563e6
Tzila-m <tzmenkes832@gmail.com>
2025-05-13 20:00:26 +0300

    10/05/20:00

---
+++ client/src/app/apiSlice.js
@@ -0,0 +1,11 @@
+const { createApi, fetchBaseQuery } = '@reduxjs/toolkit/query/react';
+
+const apiSlice = createApi({
+   
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'http://localhost:1100' 
+    }),
+
+    endPoints: () => ({})
+});
